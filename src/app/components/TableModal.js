220e72192b0610e0bed1dfdfa607c122fcb15ef4
davitpra <davitpra@Davit>
2024-11-12 14:55:19 -0500

    changing Link into TableModal

--- src/app/components/TableModal.js
+++ src/app/components/TableModal.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useTableModal } from "../hooks/useTableModal";
 import { useTableNumber } from "../hooks/useTableNumber";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export function TableModal({ residents }) {
   const route = useRouter();
@@ -31,11 +32,11 @@ export function TableModal({ residents }) {
           </h2>
           <div className="grid grid-cols-2 gap-4">
             {ResidentsOnTable.map((resident) => (
-              <button
+              <Link
                 key={resident.id}
+                href={`./room/${resident.roomId}`}
                 className="bg-gray-100 p-2 rounded text-sm flex flex-col justify-center items-center"
                 onClick={() => {
-                  route.push(`./room/${resident.roomId}`);
                   tableModal.onClose();
                 }}
               >
@@ -46,7 +47,7 @@ export function TableModal({ residents }) {
                   {resident.name} {resident.lastName}
                 </h3>
                 <p className="text-xs pl-4">Room: {resident.roomId}</p>
-              </button>
+              </Link>
             ))}
           </div>
         </div>
