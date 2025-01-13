3687d79dfb36f5be8237fc5817188a32f2fc55e3
davitpra <davitpra@Davit>
2025-01-13 15:37:13 -0500

    change of the lib and the page room id

--- src/app/room/[roomId]/page.js
+++ src/app/room/[roomId]/page.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { residents } from "../../../data/residents";
 import { Wraper } from "@/components/Wraper";
 import {
   Disclosure,
@@ -10,32 +9,66 @@ import {
   TabPanels,
 } from "@headlessui/react";
 import { UserIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
-
+import { getAllResidents } from "@/lib/get-all-residents";
 
 async function roomId({ params }) {
   const { roomId } = await params;
-  const resident = residents.filter(
-    (resident) => resident.roomId === Number(roomId)
-  );
+  const residents = await getAllResidents();
+  const resident = residents.find((resident) => resident.roomId === Number(roomId));
 
-  const fullName = `${resident[0].name} ${resident[0].lastName}`;
-  const Breakfast = resident[0].meals[0];
-  const Lunch = resident[0].meals[1];
-  const Dinner = resident[0].meals[2];
+  if (!resident) {
+    return <div>Resident not found</div>;
+  }
+
+  const { full_name: fullName, meals, Picture, Service_Notes } = resident;
+  const [Breakfast, Lunch, Dinner] = meals;
+
+  const renderMealDetails = (meal, mealName) => (
+    <Disclosure as="div">
+      <h3>
+        <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
+          <span className="text-sm font-medium text-gray-900 group-data-[open]:text-indigo-600">
+            {mealName}
+          </span>
+          <span className="ml-6 flex items-center">
+            <PlusIcon
+              aria-hidden="true"
+              className="block h-6 w-6 text-gray-400 group-hover:text-gray-500 group-data-[open]:hidden"
+            />
+            <MinusIcon
+              aria-hidden="true"
+              className="hidden h-6 w-6 text-indigo-400 group-hover:text-indigo-500 group-data-[open]:block"
+            />
+          </span>
+        </DisclosureButton>
+      </h3>
+      <DisclosurePanel className="prose prose-sm pb-6">
+        <div className="mt-2 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            {Object.entries(meal).map(([key, value]) => (
+              <div key={key} className="py-2 grid grid-cols-3 gap-4 px-0">
+                <dt className="text-sm/6 font-medium text-gray-900">{key}</dt>
+                <dd className="text-sm/6 text-gray-700 col-span-2 mt-0">{value}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
 
   return (
     <Wraper>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 md:max-w-7xl md:px-8">
           <div className="sm:grid sm:grid-cols-2 sm:items-start sm:gap-x-8">
-            {/* Image gallery */}
             <TabGroup className="flex flex-col-reverse">
               <TabPanels className="aspect-h-1 aspect-w-1 w-full">
-                <TabPanel>
-                  {resident[0].image ? (
+                <TabPanel className="aspect-h-1 aspect-w-1">
+                  {Picture ? (
                     <img
                       alt={`${fullName} image`}
-                      src={resident[0].image}
+                      src={Picture.url}
                       className="h-full w-full object-cover object-center sm:rounded-lg"
                     />
                   ) : (
@@ -48,160 +81,29 @@ async function roomId({ params }) {
               </TabPanels>
             </TabGroup>
 
-            {/* Product info */}
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               <div className="flex flex-col sm:flex-row sm:justify-evenly">
-                <h1 className="text-2xl font-bold tracking-tight text-gray-900">
-                  {fullName}
-                </h1>
-                <p className="hidden sm:inline text-3xl font-bold tracking-tight text-gray-900">
-                  -
-                </p>
-                <h2 className="sm:text-2xl font-bold tracking-tight text-gray-900">
-                  Room {roomId}
-                </h2>
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900">{fullName}</h1>
+                <p className="hidden sm:inline text-3xl font-bold tracking-tight text-gray-900">-</p>
+                <h2 className="sm:text-2xl font-bold tracking-tight text-gray-900">Room {roomId}</h2>
               </div>
               <div className="mt-6">
                 <h3 className="font-semibold pb-4">Observations</h3>
-
                 <div className="space-y-6 text-base text-gray-700">
-                  <ul className="space-y-2 list-disc list-inside">
-                    {resident[0].observations.map((observation, key) => (
-                      <li key={key}>{observation}</li>
-                    ))}
-                  </ul>
+                  {Service_Notes.split("\n").map((note, index) => (
+                    <ul key={index} className="space-y-2 list-disc list-inside">
+                      <li>{note}</li>
+                    </ul>
+                  ))}
                 </div>
               </div>
 
               <section aria-labelledby="details-heading" className="mt-12">
-                <h2 id="details-heading" className="sr-only">
-                  Meals details
-                </h2>
-
+                <h2 id="details-heading" className="sr-only">Meals details</h2>
                 <div className="divide-y divide-gray-200 border-t">
-                    {/* Breakfast */}
-                    <Disclosure  as="div">
-                      <h3>
-                        <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
-                          <span className="text-sm font-medium text-gray-900 group-data-[open]:text-indigo-600">
-                            Breakfast
-                          </span>
-                          <span className="ml-6 flex items-center">
-                            <PlusIcon
-                              aria-hidden="true"
-                              className="block h-6 w-6 text-gray-400 group-hover:text-gray-500 group-data-[open]:hidden"
-                            />
-                            <MinusIcon
-                              aria-hidden="true"
-                              className="hidden h-6 w-6 text-indigo-400 group-hover:text-indigo-500 group-data-[open]:block"
-                            />
-                          </span>
-                        </DisclosureButton>
-                      </h3>
-                      <DisclosurePanel className="prose prose-sm pb-6">
-                        <div>
-                          <div className="mt-2 border-t border-gray-100">
-                            <dl className="divide-y divide-gray-100">
-                              {Object.entries(Breakfast).map(([key, value]) => (
-                                <div
-                                  key={key}
-                                  className="py-2 grid grid-cols-3 gap-4 px-0"
-                                >
-                                  <dt className="text-sm/6 font-medium text-gray-900">
-                                    {key}
-                                  </dt>
-                                  <dd className="text-sm/6 text-gray-700 col-span-2 mt-0">
-                                    {value}
-                                  </dd>
-                                </div>
-                              ))}
-                            </dl>
-                          </div>
-                        </div>
-                      </DisclosurePanel>
-                    </Disclosure>
-                    {/* Lunch */}
-                    <Disclosure  as="div">
-                      <h3>
-                        <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
-                          <span className="text-sm font-medium text-gray-900 group-data-[open]:text-indigo-600">
-                            Lunch
-                          </span>
-                          <span className="ml-6 flex items-center">
-                            <PlusIcon
-                              aria-hidden="true"
-                              className="block h-6 w-6 text-gray-400 group-hover:text-gray-500 group-data-[open]:hidden"
-                            />
-                            <MinusIcon
-                              aria-hidden="true"
-                              className="hidden h-6 w-6 text-indigo-400 group-hover:text-indigo-500 group-data-[open]:block"
-                            />
-                          </span>
-                        </DisclosureButton>
-                      </h3>
-                      <DisclosurePanel className="prose prose-sm pb-6">
-                        <div>
-                          <div className="mt-2 border-t border-gray-100">
-                            <dl className="divide-y divide-gray-100">
-                              {Object.entries(Lunch).map(([key, value]) => (
-                                <div
-                                  key={key}
-                                  className="py-2 grid grid-cols-3 gap-4 px-0"
-                                >
-                                  <dt className="text-sm/6 font-medium text-gray-900">
-                                    {key}
-                                  </dt>
-                                  <dd className="text-sm/6 text-gray-700 col-span-2 mt-0">
-                                    {value}
-                                  </dd>
-                                </div>
-                              ))}
-                            </dl>
-                          </div>
-                        </div>
-                      </DisclosurePanel>
-                    </Disclosure>
-                    {/* Dinner */}
-                    <Disclosure  as="div">
-                      <h3>
-                        <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
-                          <span className="text-sm font-medium text-gray-900 group-data-[open]:text-indigo-600">
-                            Dinner
-                          </span>
-                          <span className="ml-6 flex items-center">
-                            <PlusIcon
-                              aria-hidden="true"
-                              className="block h-6 w-6 text-gray-400 group-hover:text-gray-500 group-data-[open]:hidden"
-                            />
-                            <MinusIcon
-                              aria-hidden="true"
-                              className="hidden h-6 w-6 text-indigo-400 group-hover:text-indigo-500 group-data-[open]:block"
-                            />
-                          </span>
-                        </DisclosureButton>
-                      </h3>
-                      <DisclosurePanel className="prose prose-sm pb-6">
-                        <div>
-                          <div className="mt-2 border-t border-gray-100">
-                            <dl className="divide-y divide-gray-100">
-                              {Object.entries(Dinner).map(([key, value]) => (
-                                <div
-                                  key={key}
-                                  className="py-2 grid grid-cols-3 gap-4 px-0"
-                                >
-                                  <dt className="text-sm/6 font-medium text-gray-900">
-                                    {key}
-                                  </dt>
-                                  <dd className="text-sm/6 text-gray-700 col-span-2 mt-0">
-                                    {value}
-                                  </dd>
-                                </div>
-                              ))}
-                            </dl>
-                          </div>
-                        </div>
-                      </DisclosurePanel>
-                    </Disclosure>
+                  {renderMealDetails(Breakfast, "Breakfast")}
+                  {renderMealDetails(Lunch, "Lunch")}
+                  {renderMealDetails(Dinner, "Dinner")}
                 </div>
               </section>
             </div>
--- src/lib/get-all-residents.js
+++ src/lib/get-all-residents.js
@@ -2,7 +2,7 @@ import { query } from "./strapi";
 
 export async function getAllResidents() {
   return query(
-    "residents?populate=Picture&populate=Breakfast_preferences&populate=Lunch_preferences&populate=Supper_preferences"
+    "residents?populate[Breakfast_preferences][populate]=*&populate[Lunch_preferences][populate]=*&populate[Supper_preferences][populate]=*&populate=Picture"
   ).then((res) => {
     return res.data.map((resident) => {
       const {
@@ -15,31 +15,88 @@ export async function getAllResidents() {
         Service_Notes,
         Dietary_Guidelines,
         Picture,
-        Drink_preference,
-        breakfast_preference,
+        Breakfast_preferences,
+        Lunch_preferences,
+        Supper_preferences,
       } = resident;
 
+      //GET DAY OF WEEK
+      let today = new Date();
+      const dayOfWeek = today.getDay();
+
+      // Helper function to get the text from the comentaries
+      function getComentariesText(meal_preferences) {
+        const comentaries = meal_preferences?.Comentaries || [];
+        const texts = [];
+        comentaries.forEach((comentary) => {
+          comentary.children.forEach((child) => {
+            texts.push(child.text);
+          });
+        });
+      
+        return texts;
+      }
+
+      // Helper function to filter the valid properties
+      function filterValidProperties(obj) {
+        return Object.fromEntries(
+          Object.entries(obj).filter(
+            ([key, value]) =>
+              value !== undefined && value !== null && value !== "" && value !== false
+          )
+        );
+      }
+
+      // Array of meals preferences with the valid properties
       const meals = [
-        {
-          water: Drink_preference?.water || "",
-          hotdrink: Drink_preference?.Hotdrink || "",
-          cereals: breakfast_preference?.Cereals || "",
-          juice: Drink_preference?.Juice || "",
-          eggs: breakfast_preference?.eggs || "",
-          toast: breakfast_preference?.toast || "",
-          observation: breakfast_preference?.Comentaries || ""
-        },
-        {
-          water: Drink_preference?.water || "",
-          hotdrink: Drink_preference?.Hotdrink || "",
-          juice: Drink_preference?.Juice || "",
-        },
-        {
-          water: Drink_preference?.water || "",
-          hotdrink: Drink_preference?.Hotdrink || "",
-          juice: Drink_preference?.Juice || "",
-        }
+        filterValidProperties({
+          water: Breakfast_preferences?.Breakfast_drink_preference?.water,
+          hotdrink: Breakfast_preferences?.Breakfast_drink_preference?.Hotdrink,
+          cereals: Breakfast_preferences?.Cereals,
+          juice: Breakfast_preferences?.Breakfast_drink_preference?.Juice,
+          Milk: Breakfast_preferences?.Breakfast_drink_preference?.Milk,
+          eggs: Breakfast_preferences?.eggs,
+          toast: Breakfast_preferences?.toast,
+          FruitPlate: Breakfast_preferences?.Fruit_plate,
+          Yogurt: Breakfast_preferences?.Yogurt,
+          Muffing: Breakfast_preferences?.Muffing,
+          onTray: Breakfast_preferences?.onTray,
+          additionals: Breakfast_preferences?.additionals,
+          observation: getComentariesText(Breakfast_preferences),
+        }),
+        filterValidProperties({
+          water: Lunch_preferences?.Lunch_drink_preference?.water,
+          hotdrink: Lunch_preferences?.Lunch_drink_preference?.Hotdrink,
+          juice: Lunch_preferences?.Lunch_drink_preference?.Juice,
+          Milk: Lunch_preferences?.Lunch_drink_preference?.Milk,
+          observation: getComentariesText(Lunch_preferences),
+        }),
+        filterValidProperties({
+          water: Supper_preferences?.Lunch_drink_preference?.water,
+          hotdrink: Supper_preferences?.Lunch_drink_preference?.Hotdrink,
+          juice: Supper_preferences?.Lunch_drink_preference?.Juice,
+          Milk: Supper_preferences?.Lunch_drink_preference?.Milk,
+          observation: getComentariesText(Supper_preferences),
+        }),
       ];
+
+      // if it is Tuesday or Thursday, add pancakes to breakfast
+      if (dayOfWeek === 2 || dayOfWeek === 4) {
+        if (Breakfast_preferences?.Pancake) {
+          meals[0].Pancakes = ' Add pancakes';
+        } else {
+          meals[0].Pancakes = ' No pancakes';
+        }
+      }
+
+      // if it is Sunday or Wednesday, add bacon to breakfast
+      if (dayOfWeek === 0 || dayOfWeek === 3) {
+        if (Breakfast_preferences?.Bacon) {
+          meals[0].Bacon = ' Add Bacon';
+        } else {
+          meals[0].Bacon = ' No Bacon';
+        }
+      }
 
       return {
         id,
