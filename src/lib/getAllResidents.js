b6079e41447bd54b6aa72630624a5d6493e5f3fc
David Prado <j.david.prado1716@gmail.com>
2025-05-05 10:55:52 -0400

    Filtrar residentes que se han mudado en la función getAllResidents

--- src/lib/getAllResidents.js
+++ src/lib/getAllResidents.js
@@ -4,7 +4,7 @@ export async function getAllResidents() {
   return query(
     "residents?populate[Breakfast_preferences][populate]=*&populate[Lunch_preferences][populate]=*&populate[Supper_preferences][populate]=*&populate=Picture"
   ).then((res) => {
-    const residents = res.data
+    const residents = res.data.filter((person) => person.move_out !== true)
     return residents
   });
 }
