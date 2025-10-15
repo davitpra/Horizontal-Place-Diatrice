6f4347f0c35933a7827c8ac9f770438d06df5c2a
David Prado <j.david.prado1716@gmail.com>
2025-10-15 10:44:00 -0400

    Refactor Title component layout for improved responsiveness and button arrangement. Adjust flex properties and simplify button rendering logic to enhance user interaction across different screen sizes.

--- src/components/ui/Title.js
+++ src/components/ui/Title.js
@@ -3,54 +3,54 @@ import { Wraper } from "./Wraper";
 export default function Title({ title, observations = [], className = "", buttonAction, button2Action, button, button2, button3Action, button3 }) {
   return (
     <div className={`px-4 sm:px-6 lg:px-8 xl:px-32`}>
-      <div className={`sm:flex sm:items-center  ${className} my-4`}>
+      <div className={`md:flex md:items-center  ${className} my-4`}>
         <div className="sm:flex-auto">
           <h2 className="text-base font-semibold text-gray-900">{title}</h2>
-          <ul className="hidden md:block space-y-2 list-disc list-inside">
+          <ul className="space-y-2 list-disc list-inside">
             {observations.map((observation, key) => (
               <li className="mt-2 text-sm text-gray-700" key={key}>{observation}</li>
             ))}
           </ul>
         </div>
         {button && (
-        <div className="flex space-x-10 ">
-          <div className="ml-4 mt-2 shrink-0 flex space-x-4">
-            <button
-              type="button"
-              className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => {
-                buttonAction();
-              }
-              }
-            >
-              {button}
-            </button>
-            {button2 && (
-            <button
-              type="button"
-              className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => {
-                button2Action();
-              }
-              }
-            >
-              {button2}
-            </button>
-            )}
-            {button3 && (
+          <div className="mt-4 md:mt-0 flex justify-end space-x-10 ">
+            <div className="mt-2 shrink-0 flex space-x-4">
               <button
                 type="button"
                 className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 onClick={() => {
-                  button3Action();
+                  buttonAction();
                 }
                 }
               >
-                {button3}
+                {button}
               </button>
-            )}
+              {button2 && (
+                <button
+                  type="button"
+                  className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={() => {
+                    button2Action();
+                  }
+                  }
+                >
+                  {button2}
+                </button>
+              )}
+              {button3 && (
+                <button
+                  type="button"
+                  className="relative inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={() => {
+                    button3Action();
+                  }
+                  }
+                >
+                  {button3}
+                </button>
+              )}
+            </div>
           </div>
-        </div>
         )}
       </div>
     </div>
